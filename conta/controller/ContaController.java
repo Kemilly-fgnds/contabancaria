61b844daf587ab0c231633026984bff16f30a8dd
Dilma Fagundes de Sousa Silva <kemilly.fagundes@outlook.com>
2024-11-12 13:47:30 -0300

    Inserção de mais métodos na Classe Controller

--- conta/controller/ContaController.java
+++ conta/controller/ContaController.java
@@ -12,7 +12,12 @@ public class ContaController implements ContaRepository {
 
 	@Override
 	public void procurarPorNumero(int numero) {
+		var conta = buscarNaCollection(numero);
 
+		if (conta != null)
+			conta.visualizar();
+		else
+			System.out.println("\nA Conta número: " + numero + " não foi encontrada!");
 	}
 
 	@Override
@@ -30,12 +35,25 @@ public class ContaController implements ContaRepository {
 
 	@Override
 	public void atualizar(Conta conta) {
+		var buscaConta = buscarNaCollection(conta.getNumero());
+
+		if (buscaConta != null) {
+			listaContas.set(listaContas.indexOf(buscaConta), conta);
+			System.out.println("\nA Conta número: " + conta.getNumero() + " foi atualizada com sucesso!");
+		} else
+			System.out.println("\nA conta número: " + conta.getNumero() + " não foi encontrada!");
 
 	}
 
 	@Override
 	public void deletar(int numero) {
+		var conta = buscarNaCollection(numero);
 
+		if (conta != null) {
+			if (listaContas.remove(conta) == true)
+				System.out.println("\nA Conta numero: " + numero + " foi deletada com sucesso!");
+		} else
+			System.out.println("\nA Conta número: " + numero + " não foi encontrada!");
 	}
 
 	@Override
@@ -54,6 +72,17 @@ public class ContaController implements ContaRepository {
 	}
 
 	public int gerarNumero() {
-		return ++ numero;
+		return ++numero;
 	}
+
+	public Conta buscarNaCollection(int numero) {
+		for (var conta : listaContas) {
+			if (conta.getNumero() == numero) {
+				return conta;
+			}
+		}
+
+		return null;
+	}
+
 }
